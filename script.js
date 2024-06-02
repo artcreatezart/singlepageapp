9c5be5e12c832e6d0d8518986f6c71253be3505a
artcreatezart <abbie@Abbies-MBP.hub>
2024-06-02 20:30:32 +1200

    fullpage js set up and some styling

--- script.js
+++ script.js
@@ -18,42 +18,14 @@ $(document).ready(function () {
         fullpage_api.moveTo(number); // allows you to move sections or "jump"
     }
 
-    $('#goToSection1').click(function () {
-        moveToSection(1);
-    });
 
-    $('#goToSection2').click(function () {
+    $('#goToPokedexFilters').click(function () {
         moveToSection(2);
-    });
-
-    $('#goToSection3').click(function () {
-        moveToSection(3);
-    });
-
-    $('#goToSection4').click(function () {
-        moveToSection(4);
     });
 
     // Move to slide # - first number is sectin, second is slide - zero indexed for slides
     $('#goToSlide1').click(function () {
         fullpage_api.moveTo(1, 0);
-    });
-
-    $('#goToSlide2').click(function () {
-        fullpage_api.moveTo(1, 1);
-    });
-
-    // $('#goToSlide3').click(function() {
-    //     fullpage_api.moveTo(1, 2);
-    // });
-
-    $('#goToSlide4').click(function () {
-        fullpage_api.moveTo(1, 3);
-    });
-
-    // moveSilently:
-    $('#goToSlide3').click(function () {
-        fullpage_api.silentMoveTo(1, 2);
     });
 
 
